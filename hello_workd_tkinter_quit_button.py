7244bfe660305af50eed93dd12dc99ab5d877638
Jean Pereira <jeankpayales444@gmail.com>
2021-06-21 20:31:29 -0600

    Hello World window with quit button

---
+++ hello_workd_tkinter_quit_button.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Create the application window
+window = tk.Tk()
+
+# Create the user interface
+my_label = ttk.Label(window, text="Hello World")
+my_label.grid(row=1, column=1)
+
+quit_button = ttk.Button(window, text="Quit")
+quit_button.grid(row=2, column=1)
+quit_button["command"] = window.destroy
+
+# Start the GUI event loop
+window.mainloop()
+
